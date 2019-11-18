9a930c5faed93643dcd6ba2186c3f86907c91dbb
Alexey Kirpichenkau <alexwish@tut.by>
2019-11-18 15:39:42 +0300

    Templates for dinamically show offers and events lists

---
+++ resources/views/presult.blade.php
@@ -0,0 +1,21 @@
+
+
+    @foreach ($data as $event)
+        <a class="card event-card {{ ($event->status == 2)? "card-overlay":"" }}" href="/events/{{$event->id}}">
+            <div class="event-header">
+                <div class="event-time">
+                    <p>{{$event->date_start->format('d.m.Y')}}, <span>{{$event->date_start->format('H:i')}}</span></p>
+                </div>
+                <img src="{{$event->image}}" class="event-card-img" onError='this.onerror=null;this.src="/images/noimg.png";'>
+            </div>
+            <p class="event-location">{{$event->city->name}}</p>
+            <h3 class="event-title">{{$event->title}}</h3>
+            <div class="event-description">
+                    {!!  $event->getPreviewDesc() !!}
+            </div>
+        </a>
+    @endforeach
+
+    <div class="row justify-content-center paginator">
+        {{ $data->appends($_GET)->links() }}
+    </div>
