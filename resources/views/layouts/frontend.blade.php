646d5088cfb1c1a83a0dbd562086402b2b09bed0
Alexey Kirpichenkau <alexwish@tut.by>
2020-10-04 13:26:50 +0300

    Add favicons

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -7,7 +7,17 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="language" content="{{ app()->getLocale() }}"/>
-        <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png?v=m2nMeB4vkl">
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png?v=m2nMeB4vkl">
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png?v=m2nMeB4vkl">
+        <link rel="manifest" href="/images/favicon/site.webmanifest?v=m2nMeB4vkl">
+        <link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg?v=m2nMeB4vkl" color="#5bbad5">
+        <link rel="shortcut icon" href="/images/favicon/favicon.ico?v=m2nMeB4vkl">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-config" content="/images/favicon/browserconfig.xml?v=m2nMeB4vkl">
+        <meta name="theme-color" content="#ffffff">
+
         <link href="/css/justselect.min.css" rel="stylesheet" />
         @yield('styles')
 
