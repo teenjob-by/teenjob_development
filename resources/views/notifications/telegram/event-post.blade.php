53cc0387b3f2cfbfd912d6b51cb840f3ae77aa32
Alexey Kirpichenkau <alexwish@tut.by>
2020-10-13 04:16:20 +0300

    Fit template

--- resources/views/notifications/telegram/event-post.blade.php
+++ resources/views/notifications/telegram/event-post.blade.php
@@ -4,7 +4,7 @@
 
 ⏳ {{ $data->date_start->format('H:i') }}, {{ $data->date_start->format('d.m.Y') }}
 📍 {{ $data->city->name }}, {{ $data->address }}
-@if($data->type->name == "Платно")💰{{ $data->type->name}}@else✅{{ $data->type->name}}@endif
+@if($data->type->name == "Платно")💰 {{ $data->type->name}}@else✅ {{ $data->type->name}}@endif
 
 {!! $data->descriptionMarkdown() !!}
 📎
